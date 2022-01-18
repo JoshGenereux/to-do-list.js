b0b52c2c2091d6d9c39c544a4a2849cd1e363712
JoshGenereux <josh.genereux13@gmail.com>
2022-01-18 16:46:37 -0700

    restarted

---
+++ client/app.js
@@ -0,0 +1,31 @@
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(e){
+    e.preventDefault();
+
+    const todoDiv = document.createElement('div');
+
+    todoDiv.classList.add('todo');
+    const newTodo = document.createElement('li');
+    // create li
+    newTodo.innerText = "test";
+    newTodo.classList.add('todoItem');
+    todoDiv.appendChild(newTodo);
+    // check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv)
+}
